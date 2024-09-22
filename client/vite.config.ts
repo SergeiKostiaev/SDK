2e9a5910a441c233ab795f7be5006c4f0089fb56
Sergei <skprod.info@gmail.com>
2024-09-22 23:28:31 +0400

    change config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5174, // Устанавливаем порт на 5174
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/variables.scss";`
+      }
+    }
   },
-})
+  server: {
+    port: 5174,
+  },
+});
